be3d34ccaa4f04ef2bc516e587a42477a84ce298
rumbo <rumbo.9370@gmail.com>
2020-03-08 23:30:14 +0100

    kommtar om hvad de forskille fetch gør

--- deezer/js.js
+++ deezer/js.js
@@ -1,12 +1,6 @@
-// fetch('https://api.deezer.com/playlist/908622995')
-// fetch('https://api.deezer.com/user/2529/playlists')
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-
+// ******************************************************************
+// fetch playlist
+// ******************************************************************
 // fetch("https://deezerdevs-deezer.p.rapidapi.com/playlist/5", {
 // 	"method": "GET",
 // 	"headers": {
@@ -24,7 +18,32 @@
 // 	console.log(err);
 // });
 
-fetch("https://deezerdevs-deezer.p.rapidapi.com/track/854914322", {
+
+// ******************************************************************
+// fetch en sang
+// ******************************************************************
+// fetch('https://deezerdevs-deezer.p.rapidapi.com/track/854914322', {
+//   method: 'GET',
+//   headers: {
+//     'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
+//     'x-rapidapi-key': 'aee2130728mshc250038c75dd0d2p181ea1jsn58e75ec5d280'
+//   }
+// })
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
+// ******************************************************************
+// fetch album
+// ******************************************************************
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/album/127270232", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
@@ -32,11 +51,7 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/track/854914322", {
 	}
 })
 .then(response => {
-   return response.json();
-})
-.then(data =>{
-    console.log(data);
-    
+	console.log(response);
 })
 .catch(err => {
 	console.log(err);

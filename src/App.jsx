3508dfb6ce5bf805e87349a6e9faa2b0114d6392
rumbo <rumbo.9370@gmail.com>
2020-03-08 23:47:53 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import logo from './logo1.png';
+
+const FetchPage = props => {
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    const fetchController = new AbortController();
+    if (!apiData) {
+      const fetchHeaders = new Headers();
+      fetchHeaders.append('Accept', 'application/json');
+
+   
+// fetch('https://deezerdevs-deezer.p.rapidapi.com/track/854914322', {
+fetch('https://deezerdevs-deezer.p.rapidapi.com/album/127270232', {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
+    'x-rapidapi-key': 'aee2130728mshc250038c75dd0d2p181ea1jsn58e75ec5d280'
+  }
+})
+  .then(response => {
+    return response.json();
+  })
+  .then(data => setApiData(data))
+  .catch(err => {
+    console.log(err);
+  });
+    }
+
+    return () => fetchController.abort();
+  }, [apiData]);
+  return (
+    <div>
+    {apiData && apiData.title}
+  </div>
+  );
+};
+
+const Container = props => {
+  return <div> {props.children} </div>;
+};
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <nav className="Dez">
+          <img className="logo" src={logo} alt="logo" />
+          Hpotify
+        </nav>
+
+        <div className="todo">
+          <div className="item1">Jump back in</div>
+        </div>
+      </Container>
+      <FetchPage />
+    </div>
+  );
+}
+
+export default App;

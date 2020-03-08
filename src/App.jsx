9f4083fd75372d62db7d595ba1814e8784cea8b5
rumbo <rumbo.9370@gmail.com>
2020-03-09 00:07:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './logo1.png';
 
+
 const FetchPage = props => {
   const [apiData, setApiData] = useState(null);
 
@@ -11,9 +12,14 @@ const FetchPage = props => {
       const fetchHeaders = new Headers();
       fetchHeaders.append('Accept', 'application/json');
 
-   
+  // **********************************************
+ //  laver en fetch 
+// ************************************************
 // fetch('https://deezerdevs-deezer.p.rapidapi.com/track/854914322', {
 fetch('https://deezerdevs-deezer.p.rapidapi.com/album/127270232', {
+    // **********************************************
+ //  sætter method og headers
+// ************************************************
   method: 'GET',
   headers: {
     'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
@@ -23,7 +29,13 @@ fetch('https://deezerdevs-deezer.p.rapidapi.com/album/127270232', {
   .then(response => {
     return response.json();
   })
+  // **********************************************
+ //  sætter data til at være consten setApiData
+// ************************************************
   .then(data => setApiData(data))
+  // **********************************************
+ //  fanger hvis der er en fail
+// ************************************************
   .catch(err => {
     console.log(err);
   });

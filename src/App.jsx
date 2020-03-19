9a65bb2c7c645f856d03dceb77306615ccc1c89f
kriser21 <kristina@nebe.dk>
2020-03-19 11:06:37 +0100

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import logo from './logo1.png';
-import ReactPlayer from 'react-player'
-import { render } from '@testing-library/react';
-import './index.css'
-
+import ReactPlayer from 'react-player';
 
 const FetchPage = props => {
   const [apiData, setApiData] = useState(null);
+  // laver en state
+  const [showPlaylist, setShowPlaylist] = useState(false);
   useEffect(() => {
     if (!apiData) {
-      const fetchHeaders = new Headers();
-      fetchHeaders.append('Accept', 'application/json');
+      // const fetchHeaders = new Headers();
+      // fetchHeaders.append('Accept', 'application/json');
 
-  // **********************************************
- //  laver en fetch 
-// ************************************************
+      // **********************************************
+      //  laver en fetch
+      // ************************************************
 
-fetch('https://deezerdevs-deezer.p.rapidapi.com/album/127270232', {
+      fetch('https://deezerdevs-deezer.p.rapidapi.com/album/127270232', {
+        // **********************************************
+        //  sætter method og headers
+        // ************************************************
 
-    // **********************************************
- //  sætter method og headers
-// ************************************************
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
+          'x-rapidapi-key': 'aee2130728mshc250038c75dd0d2p181ea1jsn58e75ec5d280'
+        }
+      })
+        .then(response => {
+          return response.json();
+        })
+        // **********************************************
+        //  sætter data til at være consten setApiData
+        // ************************************************
 
-  method: 'GET',
-  headers: {
-    'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
-    'x-rapidapi-key': 'aee2130728mshc250038c75dd0d2p181ea1jsn58e75ec5d280'
-  }
-})
-  .then(response => {
-    return response.json();
-  })
-  // **********************************************
- //  sætter data til at være consten setApiData
-// ************************************************
-
-  .then(data => setApiData(data))
-  .catch(err => {
-    console.log(err);
-  });
+        .then(data => setApiData(data))
+        .catch(err => {
+          console.log(err);
+        });
     }
   });
-    // **********************************************
- // loopper igemen array til tracks
-// ************************************************
+  // **********************************************
+  // loopper igemen array til tracks
+  // ************************************************
 
-  let test2 = apiData && apiData.tracks.data.map(track => {
-    
-    return(
-      <div key={track.id}>
-        <p>{track.title}</p><p>{track.artist.name}</p>
-        <div className='player-wrapper'>
-        <ReactPlayer
-          url={track.preview}
-          className='react-player'
-          controls
-          width='300px'
-          height='50px'
-        />
-      </div>
-      
-      </div>
-    );
-  })
+  let playList =
+    apiData &&
+    apiData.tracks.data.map(track => {
+      return (
+        <div key={track.id} className="playlistInfo">
+          <p>song title: {track.title}</p>
+          <p>{track.artist.name}</p>
+            <ReactPlayer
+              url={track.preview}
+              className="react-player"
+              controls
+              width="250px"
+              height="50px"
+            />
+          </div>
+      );
+    });
+
   return (
-    <section className="grid_wrapper">
-    <div className="wrapper">
-    <img src={apiData && apiData.cover_medium} alt="" onClick={test2}/>
-    <img src={apiData && apiData.cover_medium} alt="" />
-  {/* <div>{apiData && apiData.tracks.data[0].title}</div> */}
-  {test2}
-  </div>
-  </section>
+    <section>
+        {/* laver en onclick der bruger den state til at vise playlisten og game den igen */}
+        <img
+          className="playList1"
+          src={apiData && apiData.cover_medium}
+          alt=""
+          onClick={() => setShowPlaylist(!showPlaylist)}
+        />
+        {showPlaylist && (
+          <div className="test">
+            <p onClick={() => setShowPlaylist(!showPlaylist)} className="close">x</p> {playList}
+          </div>
+        )}
+    </section>
   );
 };
-
-
-// const Player = () => {
-//     return (
-//       <div className='player-wrapper'>
-//         <ReactPlayer
-//           url="https://cdns-preview-d.dzcdn.net/stream/c-de98e71ba0560593f855a280e96565ec-4.mp3"
-//           className='react-player'
-//           controls
-//           width='300px'
-//           height='100px'
-//         />
-//       </div>
-//     )
-// }
-
-
-
 
 const Container = props => {
   return <div> {props.children} </div>;
@@ -110,17 +96,6 @@ function App() {
         <div className="todo">
           <div className="item1">Jump back in</div>
         </div>
-
-        <div className= "Test">
-        <div className="box"></div>
-        <div className="box1"></div>
-        <div className="box2"></div>
-        <div className="box3"></div>
-        
-        </div>
-
-
-  
       </Container>
       <FetchPage />
     </div>
